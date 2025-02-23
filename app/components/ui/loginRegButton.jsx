8fd1bb0fb8b7a61936c91be1f01488d9f8a1349a
Md. Shah Paran <paranishere@gmail.com>
2025-02-23 20:19:15 +0600

    fix: image loading error using NextImage & Login Register button restructure

---
+++ app/components/ui/loginRegButton.jsx
@@ -0,0 +1,22 @@
+
+import Link from "next/link";
+
+export default function loginRegButton() {
+  return (
+    <div className="flex h-screen bg-zinc-400 justify-center items-center gap-6">
+      <Link
+        href="/login"
+        className="bg-teal-500 text-whit px-4 py-2 text-lg rounded-md shadow-sm"
+      >
+        Login
+      </Link>
+      <Link
+        href="/sign-up"
+        className="bg-purple-500 text-whit px-4 py-2 text-lg rounded-md shadow-sm"
+      >
+        Sign-up
+      </Link>
+    </div>
+  );
+}
+
