7d5ceb3c9046cc2dc6a1c931cd54090bb56c368c
Tanvir <tanvir.info07@gmail.com>
2025-02-26 00:26:13 +0600

    fiximgurl: img url have been filled with the correct product

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import Link from "next/link";
 
 export default async function Home() {
   // fetch product data
-  const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
+  const baseUrl =
+    process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
   const response = await fetch(`${baseUrl}/products`);
   const { products } = await response.json();
 
@@ -18,31 +19,39 @@ export default async function Home() {
           id="product-container"
           className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
         >
-          {products && products.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-auto lg:h-80">
-                <Image
-                  src={product.image}
-                  alt={product.name}
-                  className="object-cover object-center"
-                  width={500}
-                  height={500}
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-900">
-                    <Link href={`/products/${product.id}`}>
-                      <span aria-hidden="true" className="absolute inset-0"></span>
-                      {product.name}
-                    </Link>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.description}</p>
+          {products &&
+            products.map((product) => (
+              <div key={product.id} className="group relative">
+                <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-auto lg:h-80">
+                  <Image
+                    src={product.image}
+                    alt={product.name}
+                    className="object-cover object-center"
+                    width={500}
+                    height={500}
+                  />
                 </div>
-                <p className="text-sm font-medium text-gray-900">${product.price}</p>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-900">
+                      <Link href={`/products/${product.id}`}>
+                        <span
+                          aria-hidden="true"
+                          className="absolute inset-0"
+                        ></span>
+                        {product.name}
+                      </Link>
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">
+                      {product.description}
+                    </p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">
+                    ${product.price}
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
