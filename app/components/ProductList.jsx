87a29ddf9792267780b98256828a7aa47ba3ef56
Tanvir <tanvir.info07@gmail.com>
2025-03-03 00:15:29 +0600

    search and filter complete

--- app/components/ProductList.jsx
+++ app/components/ProductList.jsx
@@ -2,10 +2,13 @@
 import { IoIosArrowDown } from "react-icons/io";
 import ProductCard from "./ProductCard";
 import { useState } from "react";
-import { getAllCategorys } from "@/utils";
+import { getAllCategorys, getFilterProducts, getSearchProducts } from "@/utils";
+import ProductSearch from "./ProductSearch";
+import FilterProduct from "./FilterProduct";
 const ProductList = ({ products }) => {
   const [selectedCat, SetSelectedCat] = useState([]);
   const [searchText, setSearchText] = useState("");
+  const [toggle, setToggle] = useState(false);
   const categorys = getAllCategorys(products);
 
   const handelChange = (e) => {
@@ -15,58 +18,32 @@ const ProductList = ({ products }) => {
       checked ? [...prev, value] : prev?.filter((item) => item !== value)
     );
   };
+  const handelToggle = () => {
+    setToggle((prev) => !prev);
+  };
 
-  const filterProduct =
-    selectedCat?.length === 0
-      ? products
-      : products.filter((produ) => selectedCat?.includes(produ?.category));
-
-  const finalProduct = filterProduct?.filter((product) =>
-    product?.name
-      ?.toLocaleLowerCase()
-      ?.includes(searchText?.toLocaleLowerCase())
-  );
+  const filterProduct = getFilterProducts(selectedCat, products);
+  const finalProduct = getSearchProducts(filterProduct, searchText);
 
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div className="flex justify-between">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          <h2 className="md:text-2xl text-xl font-bold tracking-tight text-gray-900">
             Product List
           </h2>
-          <div className="flex gap-6 items-center justify-center">
-            <div>
-              <input
-                type="text"
-                value={searchText}
-                onChange={(e) => setSearchText(e.target.value)}
-                placeholder="Search product"
-                className="outline-teal-500 border-none px-3 py-2 text-base text-black ring-1 rounded-sm"
-              />
-            </div>
-            <div className="relative">
-              <span className="text-black text-base cursor-pointer">
-                Filter
-                <IoIosArrowDown className="inline-block ml-2 font-semibold text-base text-black" />
-              </span>
-              <div className="absolute top-10 z-10 right-0 bg-gray-200 rounded-md w-[200px] p-5">
-                {categorys?.map((cat, i) => (
-                  <label
-                    key={i}
-                    className="inline-flex w-full cursor-pointer hover:bg-gray-50 items-center px-4 py-2 text-sm text-gray-700"
-                  >
-                    <input
-                      type="checkbox"
-                      className="form-checkbox h-4 w-4"
-                      value={cat}
-                      checked={selectedCat.includes(cat)}
-                      onChange={handelChange}
-                    />
-                    <span className="ml-2">{cat}</span>
-                  </label>
-                ))}
-              </div>
-            </div>
+          <div className="flex gap-6 flex-col md:flex-row md:items-center justify-center">
+            <ProductSearch
+              searchText={searchText}
+              setSearchText={setSearchText}
+            />
+            <FilterProduct
+              categorys={categorys}
+              selectedCat={selectedCat}
+              handelChange={handelChange}
+              toggle={toggle}
+              onToggle={handelToggle}
+            />
           </div>
         </div>
 
